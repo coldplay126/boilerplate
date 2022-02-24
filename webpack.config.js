45626ae06c09bee4014373e37fed51b7c6ea9073
coldplay126 <68833110+coldplay126@users.noreply.github.com>
2022-02-24 11:45:39 +0900

    first commit
    - 초기 boilerplate

---
+++ webpack.config.js
@@ -0,0 +1,65 @@
+require("dotenv").config();
+
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+const isProd = process.env.NODE_ENV === "production";
+const PORT = process.env.PORT || 3000;
+
+module.exports = {
+  entry: "./src/index.js",
+  mode: isProd ? "production" : "development",
+  devtool: isProd ? "hidden-source-map" : "source-map",
+  output: {
+    filename: "[name].js",
+    path: path.join(__dirname, "/dist"),
+  },
+  resolve: {
+    modules: ["node_modules"],
+    extensions: [".ts", ".tsx", ".js", ".jsx", "scss"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/, // checks for .js or .jsx files
+        exclude: /(node_modules)/,
+        loader: "babel-loader",
+        options: { presets: ["@babel/env"] },
+      },
+      {
+        test: /\.css$/, //checks for .css files
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(webp|jpg|png|jpeg)$/,
+        loader: "file-loader",
+        options: {
+          name: "[name].[ext]?[hash]",
+        },
+      },
+    ],
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, "public", "index.html"),
+      hash: true,
+    }),
+  ],
+
+  stats: "errors-only",
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, "public"),
+    },
+    port: PORT,
+    open: true,
+    client: {
+      overlay: true,
+    },
+    hot: true,
+    host: "localhost",
+    historyApiFallback: true,
+    compress: true,
+  },
+};
